b4c815f24dad85f62125bb88c77281ac6ccb1e2a
Chinmay Awasthi <94186244+Achinmay05@users.noreply.github.com>
2024-10-06 00:18:35 +0530

    Create Postorder_Preorder_Inorder.cpp

---
+++ Postorder_Preorder_Inorder.cpp
@@ -0,0 +1,64 @@
+// DFS(Inorder, Preorder, Postorder)
+
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void Print(Node* root){
+    cout << root -> data << endl;
+    cout << root -> right -> data << endl;
+    cout << root -> left -> data << endl;
+}
+
+void Inorder(Node* node){
+    if(node == NULL){
+        return;
+    }
+    
+    Inorder(node -> left);
+    cout << node->data;
+    Inorder(node -> right);
+}
+
+void Postorder(Node* node){
+    if(node == NULL){
+        return;
+    }
+    
+    Inorder(node -> left);
+    Inorder(node -> right);
+    cout << node->data;
+}
+
+void Preorder(Node* node){
+    if(node == NULL){
+        return;
+    }
+    
+    cout << node->data;
+    Inorder(node -> left);
+    Inorder(node -> right);
+}
+
+int main(){
+    Node* root = new Node(1);
+    root -> left = new Node(2);
+    root -> right = new Node(3);
+    
+    // root -> left -> right = new Node(5);
+    // Print(root);
+    Inorder(root);
+    Postorder(root);
+    Preorder(root);
+}
